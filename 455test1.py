a2db203ff225bb4187deb4b6826e8a53482e8861
sso04serge <sso04@mail.aub.edu>
2021-11-15 14:01:24 +0200

    P

--- 455test1.py
+++ 455test1.py
@@ -1,6 +1,23 @@
+from extended_euclid import extended_euclid
+
 a = int(input("input a: "))
 b = int(input("input b: "))
 P = input("input plaintext: ")
+
+# def extended_euclid(m,b):
+#     A = [1,0,m]
+#     B = [0,1,b]
+#     print(A,B)
+#     while(not(B[2] == 1) and not ( B[2] == 0)):
+#         T = B
+#         Q = A[2]//B[2]
+#         B = [A[0]-Q*B[0],A[1]-Q*B[1],A[2]-Q*B[2]]
+#         A = T
+#         print(A,B)
+#     if(B[2] == 0):
+#         print("No inverse")
+#     elif(B[2] == 1):
+#         return B[1]+m
 
 def encryptaffinecipher(a, b ,P):
     output = ""
@@ -8,10 +25,13 @@ def encryptaffinecipher(a, b ,P):
         output += chr((a*(ord(P[i])-97) + b)%26 + 97)
     return output
 
-# def decryptaffinecipher(a, b, C)
-#     output = ""
-#     for i in range(len(C)):
-#         output += chr(ExtendedEuclideAlgorithm(a)*((ord(C[i]-97)-b))%26 + 97)
+def decryptaffinecipher(a, b, C):
+    output = ""
+    for i in range(len(C)):
+        output += chr((extended_euclid(26,a)*((ord(C[i])-97)-b))%26 + 97)
+    return output
 
 
 print(encryptaffinecipher(a,b,P))
+C = input("input ciphertext: ")
+print(decryptaffinecipher(a,b,C))
--- extended_euclid.py
+++ extended_euclid.py
@@ -1,16 +1,14 @@
 def extended_euclid(m,b):
     A = [1,0,m]
     B = [0,1,b]
-    print(A,B)
+    #print(A,B)
     while(not(B[2] == 1) and not ( B[2] == 0)):
         T = B
         Q = A[2]//B[2]
         B = [A[0]-Q*B[0],A[1]-Q*B[1],A[2]-Q*B[2]]
         A = T
-        print(A,B)
+        #print(A,B)
     if(B[2] == 0):
-        print("No inverse")
+        return 0
     elif(B[2] == 1):
-        return B[1]+m
-
-print(extended_euclid(26,11))
+        return B[1]%m
