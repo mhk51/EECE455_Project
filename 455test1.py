bd615dae2e3ea3877cd5018a9565881770c49a97
sso04serge <sso04@mail.aub.edu>
2021-11-15 14:10:35 +0200

    algorithm for part 1

--- 455test1.py
+++ 455test1.py
@@ -4,21 +4,6 @@ a = int(input("input a: "))
 b = int(input("input b: "))
 P = input("input plaintext: ")
 
-# def extended_euclid(m,b):
-#     A = [1,0,m]
-#     B = [0,1,b]
-#     print(A,B)
-#     while(not(B[2] == 1) and not ( B[2] == 0)):
-#         T = B
-#         Q = A[2]//B[2]
-#         B = [A[0]-Q*B[0],A[1]-Q*B[1],A[2]-Q*B[2]]
-#         A = T
-#         print(A,B)
-#     if(B[2] == 0):
-#         print("No inverse")
-#     elif(B[2] == 1):
-#         return B[1]+m
-
 def encryptaffinecipher(a, b ,P):
     output = ""
     for i in range(len(P)):
@@ -27,8 +12,11 @@ def encryptaffinecipher(a, b ,P):
 
 def decryptaffinecipher(a, b, C):
     output = ""
-    for i in range(len(C)):
-        output += chr((extended_euclid(26,a)*((ord(C[i])-97)-b))%26 + 97)
+    if (extended_euclid(26,a) == 0):
+        output = "No inverse for a, so we cannot decrypt!"
+    else:
+        for i in range(len(C)):
+            output += chr((extended_euclid(26,a)*((ord(C[i])-97)-b))%26 + 97)
     return output
 
 
