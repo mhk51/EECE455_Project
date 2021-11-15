6181326b097c112db4b73cda06e338cce68369e6
sso04serge <sso04@mail.aub.edu>
2021-11-15 22:03:12 +0200

    Vigenere

--- 455test1.py
+++ 455test1.py
@@ -1,8 +1,8 @@
 from extended_euclid import extended_euclid
 
-a = int(input("input a: "))
-b = int(input("input b: "))
-P = input("input plaintext: ")
+# a = int(input("input a: "))
+# b = int(input("input b: "))
+# P = input("input plaintext: ")
 
 def encryptaffinecipher(a, b ,P):
     output = ""
@@ -53,10 +53,58 @@ def crackaffinecipher(letter1,letter2):
     lst[1] = b
     return lst
 
+def generatekey(string, key):
+    output = key
+    if (len(string) > len(key)):
+        for i in range(len(string) - len(key)):
+            output += key[i%len(key)]
+    return output
 
-print(encryptaffinecipher(a,b,P))
-C = input("input ciphertext: ")
-print(decryptaffinecipher(a,b,C))
-x = input("input the most common letter: ")
-y = input("input the 2nd most common letter: ")
-print(crackaffinecipher(x,y))
+def Encryptvigenere(string, key):
+    output = ""
+    for i in range(len(string)):
+        if (ord(string[i])>= 97 and ord(string[i])<=122):
+            if (ord(key[i])>= 97 and ord(key[i])<=122):
+                output += chr((ord(string[i])-97 + ord(key[i])-97) % 26 + 97)
+            else:
+                output += chr((ord(string[i])-97 + ord(key[i])-65) % 26 + 97)
+        elif (ord(string[i])>= 65 and ord(string[i])<=90):
+            if (ord(key[i])>= 97 and ord(key[i])<=122):
+                output += chr((ord(string[i])-65 + ord(key[i])-97) % 26 + 65)
+            else:
+                output += chr((ord(string[i])-65 + ord(key[i])-65) % 26 + 65)
+        else:
+            output += string[i]
+    return output
+
+def Decryptvigenere(string, key):
+    output = ""
+    for i in range(len(string)):
+        if (ord(string[i])>= 97 and ord(string[i])<=122):
+            if (ord(key[i])>= 97 and ord(key[i])<=122):
+                output += chr((ord(string[i])-97 - (ord(key[i])-97)) % 26 + 97)
+            else:
+                output += chr((ord(string[i])-97 - (ord(key[i])-65)) % 26 + 97)
+        elif (ord(string[i])>= 65 and ord(string[i])<=90):
+            if (ord(key[i])>= 97 and ord(key[i])<=122):
+                output += chr((ord(string[i])-65 - (ord(key[i])-97)) % 26 + 65)
+            else:
+                output += chr((ord(string[i])-65 - (ord(key[i])-65)) % 26 + 65)
+        else:
+            output += string[i]
+    return output
+
+# print(encryptaffinecipher(a,b,P))
+# C = input("input ciphertext: ")
+# print(decryptaffinecipher(a,b,C))
+# x = input("input the most common letter: ")
+# y = input("input the 2nd most common letter: ")
+# print(crackaffinecipher(x,y))
+# c = input("input string: ")
+d = input("input key: ")
+
+# print(generatekey(c,d))
+# print(Encryptvigenere(c,generatekey(c,d)))
+e = input("input cipher: ")
+print(generatekey(e,d))
+print(Decryptvigenere(e,generatekey(e,d)))
